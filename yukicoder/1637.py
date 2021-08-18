ce788f42f8ee8ddf7885ec15a58b608fa973c1c9
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-08-18 21:29:31 +0900

    :sparkles: 1637.py

--- yukicoder/1637.py
+++ yukicoder/1637.py
@@ -1,3 +1,5 @@
+import sys
+sys.setrecursionlimit(10**7)
 N, Q = map(int, input().split())
 ab = [tuple(map(int, input().split())) for _ in range(N-1)]
 qx = [tuple(map(int, input().split())) for _ in range(Q)]
@@ -8,7 +10,22 @@ for a, b in ab:
     g[a-1].append(b-1)
     g[b-1].append(a-1)
 
+cnts = [0] * N
+
+
+def dfs(now, prev):
+    cnt = 1
+    for nx in g[now]:
+        if nx == prev:
+            continue
+        cnt += dfs(nx, now)
+    cnts[now] = cnt
+    return cnt
+
+
+dfs(0, -1)
+
 ans = 0
 for q, x in qx:
-    ans += N * x
+    ans += cnts[q-1] * x
     print(ans)
