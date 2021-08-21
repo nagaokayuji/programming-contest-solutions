f2d4af8225166300a0dd20bb6ae7e85c26c6be6e
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-08-22 00:42:37 +0900

    ⚡️  njit

--- abc215/e.py
+++ abc215/e.py
@@ -1,44 +1,28 @@
-from collections import defaultdict
+import numba
+import numpy as np
 N = int(input())
 S = list(input())
-
-
+A = list(map(lambda x: ord(x) - ord('A'), S))
 MOD = 998244353
 
 
-# dp = [[0] * (1 << 10) for _ in range(N+1)]
-dp = [[([0 for _ in range(10)]) for _ in range(1 << 10)] for _ in range(N+2)]
-for i in range(0, 10):
-    dp[0][0][i] = 0
+@numba.njit(cache=True)
+def solve(A):
+    dp = np.zeros((N+1, 1 << 10, 10), dtype=np.int64)
+    for i, x in enumerate(A):
+        dp[i] %= MOD
+        dp[i+1][1 << x][x] += 1
+        for subset in range(1 << 10):
+            if (subset >> x) & 1 == 0:  # x を追加する場合
+                for j in range(10):
+                    dp[i+1][subset | (1 << x)][x] += dp[i][subset][j]
+            else:  # すでにある場合
+                dp[i+1][subset | (1 << x)][x] += dp[i][subset][x]
 
-
-def od(x): return ord(x) - ord('A')
-
-
-A = list(map(od, S))
-
-
-# 10^7
-for i, x in enumerate(A):
-    dp[i+1][1 << x][x] += 1
-    dp[i+1][1 << x][x] %= MOD
-
-    for subset in range(1 << 10):
-        if (subset >> x) & 1 == 0:  # x を追加する場合
             for j in range(10):
-                if x == j:
-                    continue
-                dp[i+1][subset | (1 << x)][x] += dp[i][subset][j] % MOD
-        # すでにある場合
-        if (subset >> x) & 1 == 1:
-            dp[i+1][subset | (1 << x)][x] += dp[i][subset][x] % MOD
+                dp[i+1][subset][j] += dp[i][subset][j]   # 選ばない場合
 
-        for j in range(10):
-            dp[i+1][subset][j] += dp[i][subset][j] % MOD  # 選ばない場合
+    return dp[N].sum() % MOD
 
 
-ans = 0
-for i in range(10):
-    for j in range(1 << 10):
-        ans += dp[N][j][i] % MOD
-print(ans % MOD)
+print(solve(np.array(A)))
