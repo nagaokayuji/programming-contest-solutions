3846703af171ff83845ce75828026d85bfc2918f
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-09-04 23:41:20 +0900

    ABC217E py

--- abc217/e.py
+++ abc217/e.py
@@ -1,50 +1,19 @@
-from numba import njit
-import numpy as np
+from collections import deque
+import heapq
 
 Q = int(input())
+heap = []
+q = deque()
 
-
-class DSU:
-    def __init__(self, n: int = 0):
-        self._n = n
-        self.parent_or_size = [-1] * n
-
-    def merge(self, a: int, b: int):
-        x = self.leader(a)
-        y = self.leader(b)
-
-        if x == y:
-            return x
-
-        if -self.parent_or_size[x] < -self.parent_or_size[y]:
-            x, y = y, x
-
-        self.parent_or_size[x] += self.parent_or_size[y]
-        self.parent_or_size[y] = x
-        return x
-
-    def same(self, a: int, b: int):
-        return self.leader(a) == self.leader(b)
-
-    def leader(self, a: int):
-        parent = self.parent_or_size[a]
-        while parent >= 0:
-            if self.parent_or_size[parent] < 0:
-                return parent
-            self.parent_or_size[a], a, parent = (
-                self.parent_or_size[parent],
-                self.parent_or_size[parent],
-                self.parent_or_size[self.parent_or_size[parent]]
-            )
-
-        return a
-
-    def size(self, a: int):
-        return -self.parent_or_size[self.leader(a)]
-
-    def groups(self):
-        leader_buf = [self.leader(i) for i in range(self._n)]
-        result = [[] for _ in range(self._n)]
-        for i in range(self._n):
-            result[leader_buf[i]].append(i)
-        return list(filter(lambda r: r, result))
+for _ in range(Q):
+    query = tuple(map(int, input().split()))
+    if query[0] == 1:
+        q.append(query[1])
+    elif query[0] == 2:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(q.popleft())
+    else:
+        while q:
+            heapq.heappush(heap, q.pop())
