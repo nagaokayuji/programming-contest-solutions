acb69c08c4e5899f0e2736bd8f1e1c32a544796d
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-22 01:41:33 +0900

    17

--- archive/typical90/17.py
+++ archive/typical90/17.py
@@ -1,11 +1,7 @@
 from pprint import pprint
 import sys
-from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-#from numba import njit, b1, i1, i4, i8, f8
+
+
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
 def li(): return list(mi())
@@ -22,7 +18,7 @@ class FenwickTree:
         initialize n length
         '''
         self._n = n
-        self.data = [0] * n
+        self.data = [0]*n
 
     def add(self, p: int, x):
         '''
@@ -52,14 +48,14 @@ class FenwickTree:
 N, M = mi()
 
 LR = [tuple(mi()) for _ in range(M)]
-LR.sort(key=lambda x: x[1])
+LR.sort(key=lambda x: (x[1], -x[0]))
 
 t1 = FenwickTree()
 t2 = FenwickTree()
 
 ans = 0
 for l, r in LR:
-    ans += t1._sum(l-1) - t2._sum(l)
+    ans += t1._sum(l) - t2._sum(l+1)
     t1.add(l, 1)
     t2.add(r, 1)
 
