2be23f0dffa601f1f29596950d1c6a1cd92fe258
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-23 10:57:06 +0900

    20

---
+++ archive/typical90/20.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+import sys
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
+from math import sqrt, gcd, factorial
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, heappushpop, heapreplace
+#from numba import njit, b1, i1, i4, i8, f8
+def input(): return sys.stdin.readline().rstrip()
+def mi(): return map(int, input().split())
+def li(): return list(mi())
+
+
+a, b, c = mi()
+
+
+if a < c**b:
+    print("Yes")
+else:
+    print("No")
