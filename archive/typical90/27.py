3227693cae8fc0a9aedb3dc36a41e4eec34d7d78
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-25 20:01:43 +0900

    27

--- archive/typical90/27.py
+++ archive/typical90/27.py
@@ -1,13 +1,5 @@
-from pprint import pprint
-import sys
-def input(): return sys.stdin.readline().rstrip()
-def mi(): return map(int, input().split())
-def li(): return list(mi())
-
-
 N = int(input())
 S = [input() for _ in range(N)]
-
 
 st = set()
 for i, s in enumerate(S):
