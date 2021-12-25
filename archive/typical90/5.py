3e6bb05b9f302c348d9af966e1a50b1ed287e24b
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-25 17:11:53 +0900

    5 1

--- archive/typical90/5.py
+++ archive/typical90/5.py
@@ -1,15 +1,26 @@
 from pprint import pprint
 import sys
 from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-#from numba import njit, b1, i1, i4, i8, f8
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
 def li(): return list(mi())
 
 
+MOD = 10**9 + 7
 N, B, K = mi()
-c = mi()
+c = li()
+
+# dp[i,j] := 上からi桁目、mod B = j の数
+dp = defaultdict(int)
+
+
+dp[0, 0] = 1
+for i in range(N):
+    for j in range(B):
+        for k in range(K):
+            next = (10*j+c[k]) % B
+            dp[i+1, next] = (dp[i+1, next] + dp[i, j]) % MOD
+
+# pprint(dp)
+
+print(dp[N, 0])
