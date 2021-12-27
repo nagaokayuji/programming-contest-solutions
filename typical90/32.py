5fd013554960ec2a688a8ea041374fdefa4673cc
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-28 03:42:52 +0900

    32 numba

--- typical90/32.py
+++ typical90/32.py
@@ -1,15 +1,50 @@
+import numpy as np
 from pprint import pprint
 import sys
-from collections import defaultdict, Counter, deque
 from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-# from numba import njit, b1, i1, i4, i8, f8
+from numba import jit, njit, b1, i1, i4, i8, f8
+INF = 10**16
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
 def li(): return list(mi())
 
 
 N = int(input())
-A = [li() for _ in range(N)]
+A = np.array([li() for _ in range(N)])
+M = int(input())
+XY = np.array([li() for _ in range(M)])
+ok = np.array([[True] * N for _ in range(N)])
+
+if N == 1:
+    print(A[0][0])
+    exit()
+
+for x, y in XY:
+    ok[x-1][y-1] = False
+    ok[y-1][x-1] = False
+perm = np.array(list(permutations(range(N))), dtype=np.int64)
+
+
+@jit("i8(i8, i8[:,:], b1[:,:], i8[:,:])")
+def solve(N, A, ok, perm):
+    ans = INF
+    for permi in range(len(perm)):
+        order = perm[permi]
+        time = 0
+        bad = False
+        for i in range(N-1):
+            if not ok[order[i]][order[i+1]]:
+                bad = True
+                break
+        if bad:
+            continue
+        for i in range(N):
+            time += A[order[i]][i]
+        ans = min(ans, time)
+
+    if ans == INF:
+        return -1
+    return ans
+
+
+print(solve(N, A, ok, perm))
