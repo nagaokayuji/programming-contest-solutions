c94590adc5668392c0fd63711678265caadc0c47
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-28 01:25:34 +0900

    31 wip

---
+++ typical90/31.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+import sys
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
+from math import sqrt, gcd, factorial
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify, heappushpop, heapreplace
+from numba import njit, b1, i1, i4, i8, f8
+def input(): return sys.stdin.readline().rstrip()
+def mi(): return map(int, input().split())
+def li(): return list(mi())
+
+
+N = int(input())
+W = li()
+B = li()
+grundy = [[None]*1444 for _ in range(55)]
+
+for w in range(52):
+    for b in range(1442):
