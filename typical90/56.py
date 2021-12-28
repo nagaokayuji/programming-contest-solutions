31900f7674048922494ba0147b60fe19b28dfed7
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-29 04:11:40 +0900

    56 numba todo

--- typical90/56.py
+++ typical90/56.py
@@ -1,12 +1,7 @@
 from pprint import pprint
 import sys
-from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-# from numba import njit, void, b1, i1, i4, i8, f8
-# import numpy as np
+from numba import njit, void, b1, i1, i4, i8, f8
+import numpy as np
 INF = float('inf')
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
@@ -15,30 +10,39 @@ def ti(): return tuple(mi())
 
 
 N, S = mi()
-AB = [ti() for _ in range(N)]
+AB = np.array([li() for _ in range(N)])
 
-dp = [[False]*(S+1) for _ in range(N+1)]
-dp[0][0] = True
-for i, (a, b) in enumerate(AB):
-    for s in range(S+1):
-        if s+b <= S:
-            dp[i+1][s+b] |= dp[i][s]
-        if s+a <= S:
-            dp[i+1][s+a] |= dp[i][s]
 
-if not dp[N][S]:
-    print("Impossible")
-    exit()
+@njit(void(i8, i8, i8[:], i8[:]))
+def solve(N, S, A, B):
+    dp = np.zeros((N+1, S+1), np.int64)
+    dp[0][0] = 1
 
-back = []
-now = S
-for i in range(N, 0, -1):
-    a, b = AB[i-1]
-    if now >= a and dp[i-1][now-a]:
-        back.append("A")
-        now -= a
-    else:
-        back.append("B")
-        now -= b
+    for i in range(N):
+        a, b = A[i], B[i]
+        for s in range(S+1):
+            if s+b <= S:
+                dp[i+1][s+b] |= dp[i][s]
+            if s+a <= S:
+                dp[i+1][s+a] |= dp[i][s]
 
-print("".join(back[::-1]))
+    if not dp[N][S]:
+        print("Impossible")
+        return
+
+    back = [""]*N
+    now = S
+    for i in range(N, 0, -1):
+        a, b = A[i-1], B[i-1]
+        if now >= a and dp[i-1][now-a]:
+            back[i-1] = "A"
+            now -= a
+        else:
+            # back = np.append(back, "B")
+            back[i-1] = "B"
+            now -= b
+
+    print("".join(back))
+
+
+solve(N, S, AB[:, 0], AB[:, 1])
