6780f73a4c198e7a6909fc6b1c528fece6e4d7c4
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-28 14:41:59 +0900

    37

--- typical90/37.py
+++ typical90/37.py
@@ -1,32 +1,42 @@
 import sys
-INF = 10**14
+import typing
+INF = 10**13
 def input(): return sys.stdin.readline().rstrip()
 
 
 def mi(): return map(int, input().split())
 def li(): return list(mi())
+
+
 def ti(): return tuple(mi())
 
 
+def op(a, b): return max(a, b)
+
+
 class SegmentTree:
+
     def __init__(self,
                  n: int,
-                 initial: any):
+                 initial: any,
+                 op: typing.Callable[[typing.Any, typing.Any], typing.Any]):
         m = 1
         while m <= (n+1):
             m *= 2
 
         self.m = m
-        self.n = n
         self.array = [initial] * (2*m)
+        self.op = op
         self.initial = initial
 
     def update(self, index: int, val: any):
         pos = self.m + index
+        if self.array[index] == val:
+            return
         self.array[pos] = val  # update
         while pos > 1:  # update forward root
             pos >>= 1
-            self.array[pos] = max(self.array[pos*2], self.array[pos*2+1])
+            self.array[pos] = self.op(self.array[pos*2], self.array[pos*2+1])
 
     def add(self, index: int, val: any):
         self.update(index, self.array[self.m+index]+val)
@@ -35,25 +45,29 @@ class SegmentTree:
         '''
         get value of [l,r)
         '''
+        assert(l < r)
         ret = self.initial
         l += self.m
         r += self.m
         while l < r:
             if l & 1:  # その要素を使う
-                ret = max(ret, self.array[l])
+                ret = self.op(ret, self.array[l])
                 l += 1
             l >>= 1
             if r & 1:
-                ret = max(ret, self.array[r-1])
+                ret = self.op(ret, self.array[r-1])
                 r -= 1
             r >>= 1
         return ret
+
+    def get_one(self, pos: int):
+        return self.array[self.m+pos]
 
 
 W, N = mi()
 LRV = [ti() for _ in range(N)]
 
-seg = SegmentTree(n=(W+5), initial=-INF)
+seg = SegmentTree(n=(W+5), op=op, initial=-INF)
 
 
 dp = [[-INF] * (W+1) for _ in range(N+1)]
@@ -61,7 +75,6 @@ dp = [[-INF] * (W+1) for _ in range(N+1)]
 dp[0][0] = 0
 seg.update(0, 0)
 for i, (l, r, v) in enumerate(LRV):
-
     for weight in range(W+1):
         dp[i+1][weight] = dp[i][weight]
         ll = max(0, weight-r)
