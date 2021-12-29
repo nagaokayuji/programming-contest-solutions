427468f9fb50b665a2800e7b9db09e4581296214
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-29 23:07:35 +0900

    67

--- typical90/67.py
+++ typical90/67.py
@@ -1,12 +1,4 @@
-from pprint import pprint
 import sys
-from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-# from numba import njit, void, b1, i1, i4, i8, f8
-# import numpy as np
 INF = float('inf')
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
@@ -19,14 +11,12 @@ N = list(map(int, list(str(N))))
 
 
 def op(n):
-    # parse val
     val = 0
     b = 1
     while n:
         val += b * n.pop()
         b *= 8
 
-    # to 9
     ret = []
     while val:
         x = val % 9
@@ -40,8 +30,6 @@ def op(n):
 ans = N
 for _ in range(K):
     ans = op(ans)
-    if not ans:
-        print(0)
-        exit()
-
+if not ans:
+    ans.append(0)
 print("".join(map(str, ans)))
