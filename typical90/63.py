ec401ba12a6295fe266272cdda6e817b728a6bdd
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-30 04:16:22 +0900

    63

--- typical90/63.py
+++ typical90/63.py
@@ -1,12 +1,6 @@
 from pprint import pprint
 import sys
 from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-# from numba import njit, void, b1, i1, i4, i8, f8
-# import numpy as np
 INF = float('inf')
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
