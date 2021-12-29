b778cad5b6fea5986db0ae96b16f9cb65e8a5bfa
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-30 01:29:20 +0900

    68 todo

---
+++ typical90/68.py
@@ -0,0 +1,59 @@
+from pprint import pprint
+import sys
+INF = float('inf')
+def input(): return sys.stdin.readline().rstrip()
+def mi(): return map(int, input().split())
+def li(): return list(mi())
+def ti(): return tuple(mi())
+
+
+class DSU:
+    def __init__(self, n: int = 0):
+        self._n = n
+        self.parent_or_size = [-1] * n
+
+    def merge(self, a: int, b: int):
+        x = self.leader(a)
+        y = self.leader(b)
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+        return x
+
+    def same(self, a: int, b: int):
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a: int):
+        parent = self.parent_or_size[a]
+        while parent >= 0:
+            if self.parent_or_size[parent] < 0:
+                return parent
+            self.parent_or_size[a], a, parent = (
+                self.parent_or_size[parent],
+                self.parent_or_size[parent],
+                self.parent_or_size[self.parent_or_size[parent]]
+            )
+
+        return a
+
+    def size(self, a: int):
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self):
+        leader_buf = [self.leader(i) for i in range(self._n)]
+        result = [[] for _ in range(self._n)]
+        for i in range(self._n):
+            result[leader_buf[i]].append(i)
+        return list(filter(lambda r: r, result))
+
+
+N = int(input())
+Q = int(input())
+dsu = DSU(N)
+TXYV = [ti() for _ in range(Q)]
