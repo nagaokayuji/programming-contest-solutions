daa923949cf814ba9af734e559bd57ef97d1dbbe
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-12-31 20:10:01 +0900

    abc172 d numba

--- archive/abc172/d.py
+++ archive/abc172/d.py
@@ -1,13 +1,6 @@
-from pprint import pprint
 import sys
-from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial, pi, cos, sin
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
 from numba import njit, void, b1, i1, i4, i8, f8
 import numpy as np
-# from numpy import searchsorted
 INF = float('inf')
 def input(): return sys.stdin.readline().rstrip()
 def readmi(): return map(int, input().split())
@@ -18,9 +11,7 @@ def readi(): return int(input())
 
 @njit(i8(i8), cache=True)
 def solve(N):
-    table = np.zeros(N+1, dtype=np.int64)
-    for i in range(1, N+1):
-        table[i] = i
+    table = np.arange(N+1, dtype=np.int64)
     i = 2
     while i*i <= N:
         if table[i] == i:
