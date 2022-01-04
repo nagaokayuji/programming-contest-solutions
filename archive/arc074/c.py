437a7d934ba503f364dd72ca376c114a19260c88
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-04 16:42:19 +0900

    arc074 c

--- archive/arc074/c.py
+++ archive/arc074/c.py
@@ -1,13 +1,4 @@
 import sys
-from pprint import pprint
-from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial, pi, cos, sin
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-# from numba import njit, void, b1, i1, i4, i8, f8
-# import numpy as np
-# from numpy import searchsorted
 INF = float('inf')
 def input(): return sys.stdin.readline().rstrip()
 def readmi(): return map(int, input().split())
@@ -27,7 +18,6 @@ def _solve():
         return
 
     ans = INF
-    S3 = H*W/3
 
     for _ in range(2):
         s1 = b * ((a+2)//3)
@@ -45,17 +35,11 @@ def _solve():
             s2 = ((b+1)//2) * (a - (a//3))
             s3 = a*b-s1-s2
             ans = min(ans, (max(s1, s2, s3)-min(s1, s2, s3)))
-        if b//2 > 0:
-            s1 = b * (a//3)
-            s2 = (b//2) * (a - (a//3))
-            s3 = a*b-s1-s2
-            ans = min(ans, (max(s1, s2, s3)-min(s1, s2, s3)))
+
         a, b = b, a
+
     print(ans)
-    return
 
 
 if __name__ == '__main__':
-    sys.setrecursionlimit(10**8)
-    MOD = 10**9+7
     _solve()
