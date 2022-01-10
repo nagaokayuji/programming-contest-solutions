aaae6ff81775c649ecd3f8ad10fcd92d24b605cb
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-11 02:44:45 +0900

    sumitrust2019 d

--- archive/sumitrust2019/d.py
+++ archive/sumitrust2019/d.py
@@ -1,13 +1,4 @@
 import sys
-from pprint import pprint
-from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial, pi, cos, sin, hypot
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-# from numba import njit, void, b1, i1, i4, i8, f8
-# import numpy as np
-# from numpy import searchsorted
 INF = float('inf')
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
@@ -16,7 +7,7 @@ def mi1(): return map(lambda x: int(x)-1, ().split())
 
 def _solve():
     N = int(input())
-    S = input()
+    S = list(input())
 
     ans = 0
     for pin in range(1000):
