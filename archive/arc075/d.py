82e6ed465649afc08eab7eb4e7ab694a41807151
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-15 00:38:22 +0900

    arc075  d

--- archive/arc075/d.py
+++ archive/arc075/d.py
@@ -1,7 +1,24 @@
-N = int(input())
-P = float(input())
-if N >= 200:
-    print(1)
-    exit()
+from heapq import heappush, heappop, heapify, heappushpop, heapreplace
+N, A, B = map(int, input().split())
+H = [int(input()) for _ in range(N)]
 
-print(1 - pow(1-P, N))
+
+def isok(c):
+    count = 0
+    for x in H:
+        if c*B < x:
+            count += (x - B*c + A-B-1)//(A-B)
+
+    return count <= c
+
+
+ok = 10**9
+ng = 0
+while ok-ng > 1:
+    mid = (ok+ng)//2
+    if isok(mid):
+        ok = mid
+    else:
+        ng = mid
+
+print(ok)
