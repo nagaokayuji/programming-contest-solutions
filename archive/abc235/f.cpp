0178b6b4ed4c80976ee21c745151f2632c7cd888
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-16 04:42:51 +0900

    abc235 f

--- archive/abc235/f.cpp
+++ archive/abc235/f.cpp
@@ -2,25 +2,22 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
 using namespace std;
-#define FOR(i, a, n) for (int i = (int)(a); i <= (int)(n); ++i)
-#define FORR(i, a, n) for (int i = (int)(a); i >= (int)(n); --i)
-#define ALL(a)  (a).begin(),(a).end()
-#define len(a)  int((a).size())
+#define FOR(i, a, n) for (int i=(int)(a); i<(int)(n); ++i)
+#define FORR(i, a, n) for (int i=(int)(a); i>(int)(n); --i)
+#define ALL(a) (a).begin(),(a).end()
+#define len(a) int((a).size())
 #define dbg(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")\n";
-template <typename T>inline bool chmin(T &a, const T &b) {bool compare = a > b;if (a > b) a = b;return compare;}
-template <typename T>inline bool chmax(T &a, const T &b) {bool compare = a < b;if (a < b) a = b;return compare;}
-template <typename T1, typename T2>pair<T1, T2> operator+(const pair<T1, T2> &l, const pair<T1, T2> &r) {return make_pair(l.first + r.first, l.second + r.second);}
-template <typename T1, typename T2>pair<T1, T2> operator-(const pair<T1, T2> &l, const pair<T1, T2> &r) {return make_pair(l.first - r.first, l.second - r.second);}
-template <typename T>istream &operator>>(istream &is, vector<T> &vec) {for (auto &v : vec) is >> v;return is;}
-template <typename T>ostream &operator<<(ostream &os, const vector<T> &vec) {os << '[';for (auto v : vec) os << v.val() << ',';os << ']';return os;}
-template <typename T1, typename T2>ostream &operator<<(ostream &stream, const map<T1, T2>& map){for (typename map<T1, T2>::const_iterator it = map.begin();it != map.end();++it){stream << (*it).first << " --> " << (*it).second << endl;}return stream;}
-template <typename T, size_t sz>ostream &operator<<(ostream &os, const array<T, sz> &arr) {os << '[';for (auto v : arr) os << v << ',';os << ']';return os;}
+template <typename T>inline bool chmin(T &a, const T &b) {bool cmp = a > b;if (a > b) a = b;return cmp;}
+template <typename T>inline bool chmax(T &a, const T &b) {bool cmp = a < b;if (a < b) a = b;return cmp;}
+template <typename T>istream &operator>>(istream &is, vector<T> &vec) {for (auto &v: vec) is >> v;return is;}
+template <typename T>ostream &operator<<(ostream &os, const vector<T> &vec) {os << '[';for (auto v: vec)os << v << ',';os << ']';return os;}
+template <typename T1, typename T2>ostream &operator<<(ostream &stream, const map<T1, T2>& map){for (typename map<T1, T2>::const_iterator it=map.begin();it!=map.end();++it){stream<<(*it).first<<" -> "<<(*it).second<<endl;};return stream;}
+template <typename T, size_t sz>ostream &operator<<(ostream &os, const array<T, sz> &arr) {os << '[';for(auto v: arr)os<<v<<',';os <<']';return os;}
 typedef long long ll;
-using mint = atcoder::modint1000000007;
-const double PI = acos(-1.0);
-const ll MOD = 1e9 + 7;
+using mint = atcoder::modint998244353;
+ostream &operator<<(ostream &os, const vector<mint> &vec) {os << '[';for (auto v : vec) os << v.val() << ',';os << ']';return os;}
 // clang-format on
-mint dp[1010][1 << 10][2];
+
 void solve() {
     string N;
     int M;
@@ -30,46 +27,52 @@ void solve() {
     cin >> C;
 
     int ln = len(N);
-    dp[0][0][1] = 1;
-    auto pow10 = vector<mint>(1010, 1);
-    FOR(i, 1, 1009) { pow10[i] = pow10[i - 1] * 10; }
+    mint dp[1 << 10][2];
+    mint sum[1 << 10][2];
+    dp[0][1] = 1;
 
-    FOR(i, 0, ln - 1) {
+    FOR(i, 0, ln) {
         int val = N[i] - '0';
-        // dbg(val);
-        // mint pw = pow10[ln - i - 1];
-        FOR(bts, 0, (1 << 10) - 1) {
-            FOR(nxv, 0, val - 1) {
-                // if ((bts >> nxv) & 1) {
-                if (dp[i][bts][1].val() > 0)
-                    dp[i + 1][bts | (1 << nxv)][0] += dp[i][bts][1] * 10 + nxv;
-                // }
-            }
-            FOR(nxv, 0, 9) {
-                // if ((bts >> nxv) & 1) {
-                if (dp[i][bts][0].val() > 0)
-                    dp[i + 1][bts | (1 << nxv)][0] += dp[i][bts][0] * 10 + nxv;
-                // }
-            }
+        mint ndp[1 << 10][2];
+        mint nsum[1 << 10][2];
+        FOR(strict, 0, 2) {
+            FOR(d, 0, 10) {
+                if ((strict == 1) && (d > val)) {
+                    continue;
+                }
+                int nx_strict = ((strict == 1) && (val == d));
 
-            dp[i + 1][bts | (1 << val)][1] = dp[i][bts][1] * 10 + val;
+                FOR(bts, 0, (1 << 10)) {
+                    if (dp[bts][strict] == 0) continue;
+
+                    int nx_bts = bts | (1 << d);
+                    if ((bts + d) == 0) nx_bts = 0;  // zero
+
+                    ndp[nx_bts][nx_strict] += dp[bts][strict];
+                    nsum[nx_bts][nx_strict] +=
+                        sum[bts][strict] * 10 + d * dp[bts][strict];
+                }
+            }
         }
+        swap(dp, ndp);
+        swap(sum, nsum);
     }
+
     int target = 0;
     for (int x : C) {
         target |= (1 << x);
     }
-    cout << "target" << target << endl;
-    cout << dp[ln][target][0].val() << endl;
-    cout << dp[ln][target][1].val() << endl;
-    cout << (dp[ln][target][1] + dp[ln][target][0]).val() << endl;
-    cout << dp[1][0][0].val() << endl;
-    cout << dp[1][0][1].val() << endl;
+
+    mint ans = 0;
+    FOR(bits, 0, 1 << 10) {
+        if ((bits & target) == target) {
+            ans += sum[bits][0] + sum[bits][1];
+        }
+    }
+    cout << ans.val() << endl;
 }
 
 signed main() {
-    cout << setprecision(12);
-    assert(true);
     solve();
     return 0;
 }
