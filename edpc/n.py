63a60208b9171cc039485dcccee38334097ffb13
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-18 23:21:40 +0900

    ⚡️

--- edpc/n.py
+++ edpc/n.py
@@ -10,16 +10,14 @@ def mi1(): return map(lambda x: int(x)-1, input().split())
 @njit(void(i8, i8[:]))
 def solve(N, cumsum):
     dp = np.zeros((N+1, N+1), dtype=np.int64)
-    for length in range(1, N+1):
+    for length in range(2, N+1):
         for left in range(N-length+1):
             right = left+length  # [left,right)
             t = INF
-            if length == 1:
-                t = 0
             for mid in range(left+1, right):
                 t = min(t, dp[left][mid]+dp[mid][right])
             dp[left][right] = t + cumsum[right]-cumsum[left]
-    print(dp[0][N] - cumsum[N])
+    print(dp[0][N])
 
 
 def _solve():
