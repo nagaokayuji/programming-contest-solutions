4e45a0b914c8dc374f59545bca3f0bcd324fa286
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-19 03:26:24 +0900

    edpc q

--- edpc/q.py
+++ edpc/q.py
@@ -1,14 +1,4 @@
 import sys
-from pprint import pprint
-from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial, pi, cos, sin, hypot
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-# from numba import njit, void, b1, i1, i4, i8, f8
-# import numpy as np
-# from numpy import searchsorted
-INF = float('inf')
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
 def mi1(): return map(lambda x: int(x)-1, input().split())
@@ -42,17 +32,14 @@ def _solve():
     hi = [(x, i) for i, x in enumerate(h)]
     hi.sort()
     a = list(mi())
-    ft = FenwickTree(N+2)
-    dp = [0]*(N+1)
+    dp = FenwickTree(N+2)
     ans = 0
     for _, i in hi:
-        dp[i] = ft._sum(i)+a[i]
-        ft.add(i, dp[i])
-        ans = max(ans, dp[i])
-    print(ans)
+        val = dp._sum(i)+a[i]
+        dp.add(i, val)
+        ans = max(ans, val)
+    print(dp._sum(N+1))
 
 
 if __name__ == '__main__':
-    sys.setrecursionlimit(10**8)
-    MOD = 10**9+7
     _solve()
