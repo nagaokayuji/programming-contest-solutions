cfce2c2ea521bc45dc7188f31890b0dd7472cdce
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-21 02:45:13 +0900

    edpc T

--- edpc/t.py
+++ edpc/t.py
@@ -1,35 +1,28 @@
 import sys
-from numba import njit, void, b1, i1, i4, i8, f8
+# from numba import njit, void, b1, i1, i4, i8, f8
 import numpy as np
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
 def mi1(): return map(lambda x: int(x)-1, input().split())
 
 
-@njit("i8(i8, b1[:])", cache=True)
+# @njit("i8(i8, b1[:])", cache=True)
 def solve(N, largers):
     MOD = 1000000007
     dp = np.zeros((N, N+1), dtype=np.int64)
     dp[0][0] = 1
 
-    for j in range(1, N+1):
-        dp[0][j] = 1
-
     for i in range(1, N):
         larger = largers[i-1]
         cum = np.cumsum(dp[i-1]) % MOD
-        for j in range(i+1):
-            if larger:
-                dp[i][j] += cum[j-1] if j > 0 else 0
-            else:
-                dp[i][j] += cum[i-1] - (cum[j-1] if j > 0 else 0)
+        if larger:
+            dp[i, 1:i+1] = dp[i, 1:i+1] + cum[:i]
+        else:
+            dp[i, : i+1] = dp[i, : i+1] + cum[i-1]
+            dp[i, 1:i+1] -= cum[:i]
         dp[i] %= MOD
 
-    ans = 0
-    for i in range(N):
-        ans += dp[N-1][i]
-        ans %= MOD
-    return ans
+    return np.sum(dp[N-1]) % MOD
 
 
 def _solve():
