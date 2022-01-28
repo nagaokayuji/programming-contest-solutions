369efcd044e9c74aebcc555da87ee6b96f0720ed
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-29 02:15:47 +0900

    typical90 ~45

---
+++ typical90/45.cpp
@@ -0,0 +1,84 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef __LOCAL
+#define dbg(x) cerr<<#x<<" = "<<(x)<<" (L"<<__LINE__<< ")\n";
+#else
+#define dbg(x)
+#endif
+#define bit(n,m)(((n)>>(m))&1)
+#define rep(i,n)for(int i=0;i<(int)n;i++)
+#define all(v) v.begin(), v.end()
+#define each(x, A) for(auto&& x :A)
+template<class T>inline bool chmin(T &a,const T &b) {bool cmp=a>b;if(a>b)a=b;return cmp;}
+template<class T>inline bool chmax(T &a,const T &b) {bool cmp=a<b;if(a<b)a=b;return cmp;}
+template<class T>istream &operator>>(istream &is,vector<T> &vec){for(auto &v: vec)is>>v;return is;}
+template<class T>ostream &operator<<(ostream &os,const vector<T> &vec){os<<'[';for (auto v: vec)os << v << ", ";os << ']';return os;}
+template<class T1,class T2>ostream &operator<<(ostream &stream, const map<T1, T2>& map){for (auto it=map.begin();it!=map.end();++it){stream<<(*it).first<<" -> "<<(*it).second<<endl;};return stream;}
+template<class T1,class T2>ostream &operator<<(ostream &stream, const pair<T1, T2>& pair){stream<<"("<<pair.first<<", "<<pair.second<<")";return stream;}
+template<class T,size_t sz>ostream &operator<<(ostream &os, const array<T, sz> &arr) {os << '[';for(auto v: arr)os<<v<<", ";os <<']';return os;}
+template<class T>using V=vector<T>;
+using Int=long long;
+using i128=__int128_t;
+const Int LINF = 5LL<<58;
+const int INF=5<<28;
+// === ACL ====
+#include <atcoder/all>
+using mint = atcoder::modint1000000007;
+ostream &operator<<(ostream &os,const mint x){os<<x.val();return os;}
+// ============
+// clang-format on
+
+void _solve() {
+  int N, K;
+  cin >> N >> K;
+  V<V<Int>> dist(N, V<Int>(N));
+  V<pair<Int, Int>> XY;
+  rep(i, N) {
+    int x, y;
+    cin >> x >> y;
+    XY.emplace_back(x, y);
+  }
+
+  rep(i, N) {
+    rep(j, N) {
+      Int dx = (XY[i].first - XY[j].first);
+      Int dy = (XY[i].second - XY[j].second);
+      Int d = dx * dx + dy * dy;
+      dist[i][j] = dist[j][i] = d;
+    }
+  }
+
+  V<Int> ds(1 << N);
+
+  rep(i, 1 << N) {
+    rep(j, N) {
+      rep(k, N) {
+        if (bit(i, j) && bit(i, k)) {
+          chmax(ds[i], dist[j][k]);
+        }
+      }
+    }
+  }
+  V<V<Int>> dp(1 << N, V<Int>(K + 1, LINF));
+  dp[0][0] = 0;
+  for (int k = 1; k <= K; k++) {
+    rep(selected_bit, 1 << N) {
+      for (int subset = selected_bit; subset > 0;
+           subset = selected_bit & (subset - 1)) {
+        chmin(dp[selected_bit][k],
+              max(dp[selected_bit ^ subset][k - 1], ds[subset]));
+      }
+    }
+  }
+
+  cout << dp[(1 << N) - 1][K] << endl;
+}
+
+signed main() {
+  cout << setprecision(12);
+  ios::sync_with_stdio(false);
+  assert(true);
+  _solve();
+  return 0;
+}
