947bceea037701e5d305fbfb1df11e1e7b66cd56
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-29 02:26:17 +0900

    typical90 46

---
+++ typical90/46.cpp
@@ -0,0 +1,66 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef __LOCAL
+#define dbg(x) cerr<<#x<<" = "<<(x)<<" (L"<<__LINE__<< ")\n";
+#else
+#define dbg(x)
+#endif
+#define bit(n,m)(((n)>>(m))&1)
+#define rep(i,n)for(int i=0;i<(int)n;i++)
+#define all(v) v.begin(), v.end()
+#define each(x, A) for(auto&& x :A)
+template<class T>inline bool chmin(T &a,const T &b) {bool cmp=a>b;if(a>b)a=b;return cmp;}
+template<class T>inline bool chmax(T &a,const T &b) {bool cmp=a<b;if(a<b)a=b;return cmp;}
+template<class T>istream &operator>>(istream &is,vector<T> &vec){for(auto &v: vec)is>>v;return is;}
+template<class T>ostream &operator<<(ostream &os,const vector<T> &vec){os<<'[';for (auto v: vec)os << v << ", ";os << ']';return os;}
+template<class T1,class T2>ostream &operator<<(ostream &stream, const map<T1, T2>& map){for (auto it=map.begin();it!=map.end();++it){stream<<(*it).first<<" -> "<<(*it).second<<endl;};return stream;}
+template<class T1,class T2>ostream &operator<<(ostream &stream, const pair<T1, T2>& pair){stream<<"("<<pair.first<<", "<<pair.second<<")";return stream;}
+template<class T,size_t sz>ostream &operator<<(ostream &os, const array<T, sz> &arr) {os << '[';for(auto v: arr)os<<v<<", ";os <<']';return os;}
+template<class T>using V=vector<T>;
+using Int=long long;
+using i128=__int128_t;
+const Int LINF = 5LL<<58;
+const int INF=5<<28;
+// === ACL ====
+#include <atcoder/all>
+using mint = atcoder::modint1000000007;
+ostream &operator<<(ostream &os,const mint x){os<<x.val();return os;}
+// ============
+// clang-format on
+
+void _solve() {
+  Int N;
+  cin >> N;
+  V<Int> A(N), B(N), C(N);
+  cin >> A >> B >> C;
+  dbg(A);
+  dbg(B);
+  dbg(C);
+  //
+  sort(all(A));
+  sort(all(B));
+  sort(all(C));
+  V<Int> Ac(46), Bc(46), Cc(46);
+  rep(i, N) {
+    Ac[A[i] % 46]++;
+    Bc[B[i] % 46]++;
+    Cc[C[i] % 46]++;
+  }
+
+  Int ans = 0;
+  rep(i, 46) rep(j, 46) rep(k, 46) {
+    if ((i + j + k) % 46 == 0) {
+      ans += Ac[i] * Bc[j] * Cc[k];
+    }
+  }
+  cout << ans << endl;
+}
+
+signed main() {
+  cout << setprecision(12);
+  ios::sync_with_stdio(false);
+  assert(true);
+  _solve();
+  return 0;
+}
