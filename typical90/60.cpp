0a4888f11025f557ba8f27cfcd332f9a215937ed
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-30 20:15:27 +0900

    typical90 60

--- typical90/60.cpp
+++ typical90/60.cpp
@@ -1,51 +1,68 @@
 // clang-format off
 #include <bits/stdc++.h>
-#include <atcoder/all>
 using namespace std;
-// using namespace atcoder;
-typedef long long ll;
-#define FOR(i, a, n) for (int i = (int)(a); i <= (int)(n); ++i)
-#define FORR(i, a, n) for (int i = (int)(a); i >= (int)(n); --i)
-#define all(x) x.begin(), x.end()
-template <typename T>inline bool chmin(T &a, const T &b) {bool compare = a > b;if (a > b) a = b;return compare;}
-template <typename T>inline bool chmax(T &a, const T &b) {bool compare = a < b;if (a < b) a = b;return compare;}
-template <typename T1, typename T2>pair<T1, T2> operator+(const pair<T1, T2> &l, const pair<T1, T2> &r) {return make_pair(l.first + r.first, l.second + r.second);}
-template <typename T1, typename T2>pair<T1, T2> operator-(const pair<T1, T2> &l, const pair<T1, T2> &r) {return make_pair(l.first - r.first, l.second - r.second);}
-template <typename T>istream &operator>>(istream &is, vector<T> &vec) {for (auto &v : vec) is >> v;return is;}
-template <typename T>ostream &operator<<(ostream &os, const vector<T> &vec) {os << '[';for (auto v : vec) os << v << ',';os << ']';return os;}
-template<typename T1, typename T2>std::ostream &operator<<(std::ostream &stream, const std::map<T1, T2>& map){for (typename std::map<T1, T2>::const_iterator it = map.begin();it != map.end();++it){stream << (*it).first << " --> " << (*it).second << std::endl;}return stream;}
-template <typename T, size_t sz>ostream &operator<<(ostream &os, const array<T, sz> &arr) {os << '[';for (auto v : arr) os << v << ',';os << ']';return os;}
+#ifdef __LOCAL
+#define dbg(x) cerr<<#x<<" = "<<(x)<<" (L"<<__LINE__<< ")\n";
+#else
+#define dbg(x)
+#endif
+#define bit(n,m)(((n)>>(m))&1)
+#define rep(i,n)for(int i=0;i<(int)n;i++)
+#define all(v) v.begin(), v.end()
+#define each(x, A) for(auto&& x :A)
+template<class T>inline bool chmin(T &a,const T &b) {bool cmp=a>b;if(a>b)a=b;return cmp;}
+template<class T>inline bool chmax(T &a,const T &b) {bool cmp=a<b;if(a<b)a=b;return cmp;}
+template<class T>istream &operator>>(istream &is,vector<T> &vec){for(auto &v: vec)is>>v;return is;}
+template<class T>ostream &operator<<(ostream &os,const vector<T> &vec){os<<'[';for (auto v: vec)os << v << ", ";os << ']';return os;}
+template<class T1,class T2>ostream &operator<<(ostream &stream, const map<T1, T2>& map){for (auto it=map.begin();it!=map.end();++it){stream<<(*it).first<<" -> "<<(*it).second<<endl;};return stream;}
+template<class T1,class T2>ostream &operator<<(ostream &stream, const pair<T1, T2>& pair){stream<<"("<<pair.first<<", "<<pair.second<<")";return stream;}
+template<class T,size_t sz>ostream &operator<<(ostream &os, const array<T, sz> &arr) {os << '[';for(auto v: arr)os<<v<<", ";os <<']';return os;}
+template<class T>using V=vector<T>;
+using Int=long long;
+using i128=__int128_t;
+const Int LINF = 5LL<<58;
+const int INF=5<<28;
+// === ACL ====
+#include <atcoder/all>
 // clang-format on
-const ll MX = 1e6 + 5, INF = 5LL << 58, MOD = 1e9 + 7;
 
-using mint = atcoder::modint1000000007;
+void _solve() {
+  int N;
+  cin >> N;
+  V<int> A(N);
+  cin >> A;
 
-void solve() {
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    cin >> A;
-
-    auto reversedA = A;
-    reverse(all(reversedA));
-    cout << A << endl;
-    cout << reversedA << endl;
-
-    assert(true);
-    assert(false);
-    atcoder::fenwick_tree<ll> fwl(N + 1);
-    atcoder::fenwick_tree<ll> fwr(N + 1);
-    vector<ll> leftlen(N + 1);
-    vector<ll> rightlen(N + 1);
-    FOR(i, 0, N - 1) {
-        ;
-        leftlen[i] = fwl.sum(0, i) + 1;
-        fwl.add(A[i], 1);
+  auto lis = [](V<int> A) {
+    dbg(A);
+    int N = A.size();
+    V<int> dp(N + 1, INF);
+    V<int> ret(N);
+    dp[0] = 0;
+    rep(i, N) {
+      int x = A[i];
+      int lb = lower_bound(all(dp), x) - dp.begin();
+      dp[lb] = x;
+      ret[i] = lb;
     }
+    // LIS = ret.back()
+    return ret;
+  };
+
+  auto rev = A;
+  reverse(all(rev));
+  auto dp1 = lis(A);
+  auto dp2 = lis(rev);
+  dbg(dp1);
+  dbg(dp2);
+  int ans = 0;
+  rep(i, N) { chmax(ans, dp1[i] + dp2[N - i - 1] - 1); }
+  cout << ans << endl;
 }
 
 signed main() {
-    cout << setprecision(12);
-    solve();
-    return 0;
+  cout << setprecision(12);
+  ios::sync_with_stdio(false);
+  assert(true);
+  _solve();
+  return 0;
 }
