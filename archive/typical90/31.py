8c4ffcad3361c2b520150973389f580bd8f7a2a7
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-02-12 12:27:09 +0900

    31

--- archive/typical90/31.py
+++ archive/typical90/31.py
@@ -1,11 +1,7 @@
 from pprint import pprint
 import sys
 from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
-from math import sqrt, gcd, factorial
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify, heappushpop, heapreplace
-from numba import njit, b1, i1, i4, i8, f8
+# from numba import njit, b1, i1, i4, i8, f8
 def input(): return sys.stdin.readline().rstrip()
 def mi(): return map(int, input().split())
 def li(): return list(mi())
@@ -14,8 +10,32 @@ def li(): return list(mi())
 N = int(input())
 W = li()
 B = li()
-grundy = [[None]*1444 for _ in range(55)]
+# grundy = [[None]*1444 for _ in range(55)]
+grundy = defaultdict(int)
 
 for w in range(52):
     for b in range(1442):
-        pass
+        s = set()
+        if w >= 1:
+            s.add(grundy[w-1, b+w])
+        if b >= 2:
+            for i in range(1, b//2 + 1):
+                s.add(grundy[w, b-i])
+
+        def mex(s):
+            i = 0
+            while True:
+                if i not in s:
+                    return i
+                i += 1
+
+        grundy[w, b] = mex(s)
+
+ans = 0
+for w, b in zip(W, B):
+    ans ^= grundy[w, b]
+
+if ans > 0:
+    print('First')
+else:
+    print("Second")
